b0705059e399124c195d1dd274fb8e36cc94d434
RaselParvejABC <RaselParvejABC@gmail.com>
2024-07-27 00:10:41 +0600

    index.html: Our Services: Multilingual Text

--- js/UIText.js
+++ js/UIText.js
@@ -25,8 +25,92 @@ const HeroCarouselText = {
   },
 };
 
+const OurServicesText = {
+  "our-services-visa-processing-title": {
+    en: "Visa Processing",
+    bn: "ভিসা প্রসেসিং",
+  },
+  "our-services-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-student-visa-processing-title": {
+    en: "Student Visa Processing",
+    bn: "স্টুডেন্ট ভিসা প্রসেসিং",
+  },
+  "our-services-student-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-worker-visa-processing-title": {
+    en: "Worker Visa Processing",
+    bn: "ওয়ার্কার ভিসা প্রসেসিং",
+  },
+  "our-services-worker-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-business-visa-processing-title": {
+    en: "Business Visa Processing",
+    bn: "বিজনেস ভিসা প্রসেসিং",
+  },
+  "our-services-business-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-ssw-visa-processing-title": {
+    en: "SSW Visa Processing",
+    bn: "SSW ভিসা প্রসেসিং",
+  },
+  "our-services-ssw-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-airline-ticket-processing-title": {
+    en: "Airline Ticket Processing",
+    bn: "বিমান টিকেট প্রসেসিং",
+  },
+  "our-services-airline-ticket-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-european-visa-processing-title": {
+    en: "European Visa Processing",
+    bn: "ইউরোপিয়ান ভিসা প্রসেসিং",
+  },
+  "our-services-european-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-usa-visa-processing-title": {
+    en: "USA Visa Processing",
+    bn: "ইউএসএ ভিসা প্রসেসিং",
+  },
+  "our-services-usa-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-uk-visa-processing-title": {
+    en: "UK Visa Processing",
+    bn: "ইউকে ভিসা প্রসেসিং",
+  },
+  "our-services-uk-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+  "our-services-tourist-visa-processing-title": {
+    en: "Tourist Visa Processing",
+    bn: "টুরিস্ট ভিসা প্রসেসিং",
+  },
+  "our-services-tourist-visa-processing-caption": {
+    en: "You can trust our expertise in Visa Processing.",
+    bn: "ভিসা প্রসেসিংয়ে আমাদের দক্ষতার ওপরে নিশ্চিন্তে আস্থা রাখতে পারেন।",
+  },
+};
+
 const UIText = {
   ...HeroCarouselText,
+  ...OurServicesText,
   "agency-name": {
     en: "Japan Travel",
     bn: "জাপান ট্রাভেল",
